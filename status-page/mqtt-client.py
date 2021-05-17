6e19cae10c9f07097548d84bd341a3e0318af756
russery <russery@gmail.com>
2021-05-16 17:39:54 -0700

    live data and plots both working

--- status-page/mqtt-client.py
+++ status-page/mqtt-client.py
@@ -1,12 +1,15 @@
 import paho.mqtt.client as mqtt
-from time import time
+from time import time, sleep
+import json
 
 sensor_types = []
-data_store = {};
+data_store = {}
+
 
 def on_connect(client, userdata, flags, rc):
-    print("Connected with result code "+str(rc))
+    print("Connected with result code " + str(rc))
     client.subscribe("$SYS/#")
+
 
 def on_message(client, userdata, msg):
     if "sensor-" in msg.topic:
@@ -20,6 +23,7 @@ def on_message(client, userdata, msg):
         if sensor_type not in sensor_types:
             sensor_types.append(sensor_type)
 
+
 def main():
     client = mqtt.Client()
     client.on_connect = on_connect
@@ -28,16 +32,27 @@ def main():
     client.subscribe([("+/temperature", 0), ("+/humidity", 0)])
 
     last_write_time = time()
+    last_live_update_time = time()
     while True:
         client.loop()
-        if time() - last_write_time > 60*5:
+        if time() - last_live_update_time > 1:
+            last_live_update_time = time()
+            # Write latest data to JSON:
+            with open("static/data/sensor_data.json", "w") as json_file:
+                json.dump(data_store, json_file)
+        if time() - last_write_time > 60 * 5:
             last_write_time = time()
+            # Append data to log CSV:
             for sensor_type in sensor_types:
-                with open("static/data/" + sensor_type + "_data.csv", "a") as csv:
+                with open("static/data/" + sensor_type + "_data.csv",
+                          "a") as csv_file:
                     for sensor in data_store:
                         if sensor_type in data_store[sensor]:
-                            line = "{},{},{}\n".format(sensor, int(time()), data_store[sensor][sensor_type])
-                            csv.write(line)
+                            line = "{},{},{}\n".format(
+                                sensor, int(time()),
+                                data_store[sensor][sensor_type])
+                            csv_file.write(line)
+
 
 if __name__ == '__main__':
     main()
--- status-page/status-page.py
+++ status-page/status-page.py
@@ -1,20 +1,16 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
+
+
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/temperature')
-def temperature():
-    return {'upstairs': 73.0, 'downstairs': 74.0 }
-
-@app.route('/humidity')
-def humidity():
-    return {'upstairs': 30.0, 'downstairs': 32.0 }
 
 def main():
     app.run(debug=True, host='0.0.0.0')
+
 
 if __name__ == '__main__':
     main()
