16cf6276dc311b08adabd25ac5acdf006868bb1b
russery <russery@gmail.com>
2021-07-31 21:36:57 -0700

    changed pin mapping for new hardware wiring

--- air-quality-node/air-quality-node/bsp.h
+++ air-quality-node/air-quality-node/bsp.h
@@ -32,8 +32,8 @@ public:
   static constexpr bool BUTTON_PRESSED PROGMEM = 0;
 
   static constexpr HardwareSerial *AQI_SERIAL_PORT PROGMEM = &Serial2;
-  static constexpr char AQI_RESET_PIN PROGMEM = 5;
-  static constexpr char AQI_SET_PIN PROGMEM = 13;
+  static constexpr char AQI_RESET_PIN PROGMEM = 2;
+  static constexpr char AQI_SET_PIN PROGMEM = 23;
 
   static constexpr char I2C_SDA_PIN = 21;
   static constexpr char I2C_SCL_PIN = 22;
